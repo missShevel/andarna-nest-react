c43e8a0dec1aba840795169299394a65883c135d
missShevel <o.v.shevel@gmail.com>
2024-06-12 22:39:25 +0300

    Nx rpoject setup

---
+++ apps/backend/src/app/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import {common} from '@andarna/common';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getData() {
+    console.log(common());
+    
+    return this.appService.getData();
+  }
+}
